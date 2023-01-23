f00e56c98d5f365dc62d40a44e56bc38500a016c
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-01-24 00:27:36 +0100

    creation controlleur tasks et verbes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,39 @@
 // on importe aussi la methode get
 
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Put } from '@nestjs/common';
+import { Delete, Post } from '@nestjs/common/decorators';
 
 
-// le 'tasks' permet d'indiquer le chemin d'ecoute /tasks
+// le 'tasks' permet d'indiquer le chemin d'ecoute /tasks, l'uri 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    @Get()
+    getTasks() {
+        console.log('recuperer liste des tasks');
+        return 'TASKING LIST';
+
+    }
+
+    @Post()
+    addTasks() {
+        console.log('ajouter une task a la liste');
+        return 'TASKING ADDED';
+
+    }
+    @Delete()
+
+    DeleteTasks() {
+        console.log('Delete one task');
+        return 'TASKING Deleted';
+
+    }
+
+    @Put()
+    modifyTasks() {
+        console.log('Modify one task');
+        return 'TASKING MODIFY';
+
+    }
+
+}
