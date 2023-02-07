928c1937e6018a1d0e8645cfb5ed22c82e55ec97
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-02-07 14:33:54 +0100

    ee

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsersEntity } from './users/users.entity/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UserService {
+    constructor(
+        @InjectRepository(UsersEntity)
+        private readonly userRepository: Repository<UsersEntity>,
+    ) { }
+
+    async createUser(user: UsersEntity) {
+        return await this.userRepository.save(user);
+    }
+
+
+    async findOne(id_users: number): Promise<UsersEntity> {
+        const userFound = await this.userRepository.findOneBy({
+            id_users: id_users,
+        });
+        if (!userFound) {
+            throw new NotFoundException(
+                `Désolé, nous n'avons pas trouvé de user avec l'id ${id_users}`,
+            );
+        }
+        return userFound;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
