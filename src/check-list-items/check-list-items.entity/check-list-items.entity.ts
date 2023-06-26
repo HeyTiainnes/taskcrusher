b35bb57d5ac50459f1eb1acc667fcfd32c235f7e
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-06-26 16:11:28 +0200

    ze

--- src/check-list-items/check-list-items.entity/check-list-items.entity.ts
+++ src/check-list-items/check-list-items.entity/check-list-items.entity.ts
@@ -1,30 +1,17 @@
 import { TasksEntity } from "src/tasks/tasks.entity/tasks.entity";
 import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-
 @Entity('checkListItems')
-
 export class checkListItemsEntity {
     @PrimaryGeneratedColumn()
     id: number
-
     @Column({ nullable: true })
     name?: string;
-
-    // @Column()
-    // items: string;
-
     @Column({ nullable: true })
     notes?: string;
-
     @ManyToOne(() => TasksEntity, (task) => task.CheckListItemsEntity, {
         onDelete: 'CASCADE',
+        eager: true
     })
     task: TasksEntity;
 }
 
-
-// @OneToOne(() => TasksEntity)
-// @JoinColumn()
-// task: TasksEntity;
-
-// }
--- src/tasks/tasks.entity/tasks.entity.ts
+++ src/tasks/tasks.entity/tasks.entity.ts
@@ -2,62 +2,35 @@
 import { CategoriesEntity } from "src/categories/categories.entity/categories.entity";
 import { checkListItemsEntity } from "src/check-list-items/check-list-items.entity/check-list-items.entity";
 import { UsersEntity } from "src/users/Dto/users.entity/users.entity";
-
 import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
-
 @Entity('Tasks')
-
 export class TasksEntity {
-
-
     @PrimaryGeneratedColumn()
     id?: number;
-
     @Column({ nullable: true })
     designation?: string;
-
     @Column({ nullable: true })
     importance?: number;
-
-
     @Column({ nullable: true })
     dead_line?: Date;
-
-
     @Column({ nullable: true })
     duree_prevue?: Date;
-
-
     @Column({ nullable: true })
     start_date?: Date;
-
-
     @Column({ nullable: true })
     etat?: boolean;
-
-
     @Column({ nullable: true })
     notes?: string;
-
-
     @ManyToOne(() => checkListItemsEntity, (cli) => cli.task, {
         onDelete: 'CASCADE',
         eager: true,
     })
-    CheckListItemsEntity: checkListItemsEntity;
-
-
-    // @OneToOne(type => CategoriesEntity, (categorie) => categorie.Tasks)
-    // categories: CategoriesEntity[];
-
+    CheckListItemsEntity: checkListItemsEntity[];
     @ManyToOne(type => UsersEntity, (users) => users.tasks,
-
         {
-
             onDelete: 'CASCADE',
             eager: false
-
         }
     )
     @JoinColumn()
