bafd5a5cb1df13ba75802e100e8873a74bd0d012
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2023-07-09 11:45:49 +0200

    warning function is not implemented

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,56 +1,11 @@
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
-// import { TasksEntity } from './tasks.entity/tasks.entity';
-// import { CreateTasksDTO } from './Dto/createTasks.dto';
-// import { updateTaskeDto } from './Dto/updateTask.dto';
-// import { UsersEntity } from 'src/users/Dto/users.entity/users.entity'; // Importez l'entité UsersEntity
-// @Injectable()
-// export class TasksService {
-//     constructor(
-//         @InjectRepository(TasksEntity)
-//         private tasksRepository: Repository<TasksEntity>,
-//     ) { }
 
-//     async create(createTaskDto: CreateTasksDTO): Promise<TasksEntity> {
-//         const task = this.tasksRepository.create(createTaskDto);
-//         return this.tasksRepository.save(task);
-//     }
-
-//     async findAll(): Promise<TasksEntity[]> {
-//         return this.tasksRepository.find({ relations: ['checkListItems'] });
-//     }
-
-//     async findOne(id: number): Promise<TasksEntity> {
-//         return this.tasksRepository
-//             .createQueryBuilder('task')
-//             .leftJoinAndSelect('task.checkListItems', 'checkListItems')
-//             .where('task.id = :id', { id })
-//             .getOne();
-//     }
-
-//     async update(id: number, updateTaskDto: updateTaskeDto): Promise<TasksEntity> {
-//         const task = await this.findOne(id);
-//         if (!task) {
-//             // Gérer l'erreur appropriée ici (tâche non trouvée)
-//         }
-
-//         task.checkListItems = updateTaskDto.checkListItems;
-
-//         return this.tasksRepository.save(task);
-//     }
-
-//     async remove(id: number): Promise<void> {
-//         await this.tasksRepository.delete(id);
-//     }
-// }
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TasksEntity } from './tasks.entity/tasks.entity';
 import { CreateTasksDTO } from './Dto/createTasks.dto';
 import { updateTaskeDto } from './Dto/updateTask.dto';
-import { UsersEntity } from 'src/users/Dto/users.entity/users.entity'; // Importez l'entité UsersEntity
+//import { UsersEntity } from 'src/users/Dto/users.entity/users.entity'; 
 
 @Injectable()
 export class TasksService {
@@ -83,7 +38,7 @@ export class TasksService {
     async update(id: number, userId: number, updateTaskDto: updateTaskeDto): Promise<TasksEntity> {
         const task = await this.findOne(id, userId);
         if (!task) {
-            // Gérer l'erreur appropriée ici (tâche non trouvée)
+
         }
 
         task.checkListItems = updateTaskDto.checkListItems;
